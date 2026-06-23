cf121248d2b77ec8d3e386c61383b9854c655318
pradeep sagar <pradeep.sagar.ios@gmail.com>
2026-06-23 22:39:54 +0530

    Initial commit

---
+++ ExpenseTracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ExpenseTracker
+//
+//  Created by Pradeep kumar sagar on 23/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Pradeep kumar sagar on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
